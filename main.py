377f6ca674252d310b84a495652082352d235980
JakPopielarz <popielarzjakub7@gmail.com>
2021-06-01 11:55:59 +0200

    Improved starting options; enabled 'copy-paste start' back again

--- main.py
+++ main.py
@@ -4,22 +4,32 @@ from Grammar.LatexParser import LatexParser
 from Grammar.LatexDocumentVisitor import LatexDocumentVisitor
 import sys
 
+options = """
+-f, --file  specify input file path
+-o, --out   specify output file path
+While all of those options are optional, they must be used in this order.
+"""
 
 def main(argv):
-    if len(sys.argv) > 1:
-        prepared_file = prepare_input(argv[1])
-        if len(sys.argv) > 2:
-            output_name = argv[2]
-        else:
-            output_name = None
-        lexer = LatexLexer(InputStream(prepared_file))
-        stream = CommonTokenStream(lexer)
-        parser = LatexParser(stream)
-        tree = parser.latexDocument()
-        markdown_file = LatexDocumentVisitor(output_name).visit(tree)
-        print("Latex document content in Markdown: \n" + markdown_file)
+    if len(sys.argv) > 2 and (argv[1] == "-f" or argv[1] == "--file"):
+        launch_from_file(argv)
+    elif len(sys.argv) == 1 or (len(sys.argv) > 2 and (argv[1] == "-o" or argv[1] == "--out")):
+        launch_from_paste(argv)
     else:
-        print('Error : Expected a valid file')
+        print('Error : Invalid command syntax. Available options:', options)
+
+def launch_from_file(argv):
+    prepared_file = prepare_input(argv[2])
+    if len(sys.argv) > 4 and (argv[3] == "-o" or argv[3] == "--output"):
+        output_name = argv[4]
+    else:
+        output_name = None
+    lexer = LatexLexer(InputStream(prepared_file))
+    stream = CommonTokenStream(lexer)
+    parser = LatexParser(stream)
+    tree = parser.latexDocument()
+    markdown_file = LatexDocumentVisitor(output_name).visit(tree)
+    print("Latex document content in Markdown: \n" + markdown_file)
 
 def prepare_input(file_name):
     with open(file_name, 'r') as file:
@@ -27,6 +37,21 @@ def prepare_input(file_name):
         while "\\begin{document}" not in lines[0]:
             lines.pop(0)
         return ''.join(lines)
+
+def launch_from_paste(argv):
+    if len(sys.argv) > 2 and (argv[1] == "-o" or argv[1] == "--output"):
+        output_name = argv[2]
+    else:
+        output_name = None
+
+    print("Paste your LaTeX document contents (without preamble) and enter EOF:\n")
+    lexer = LatexLexer(StdinStream())
+    stream = CommonTokenStream(lexer)
+    parser = LatexParser(stream)
+    tree = parser.latexDocument()
+    markdown_file = LatexDocumentVisitor(output_name).visit(tree)
+    print("Latex document content in Markdown: \n" + markdown_file)
+
 
 # run, paste one example from examples.txt, and click ^D
 if __name__ == '__main__':
