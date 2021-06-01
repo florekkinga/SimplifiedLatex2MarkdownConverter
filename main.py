5a229c5f10d2c0c4044829369ce0c82f4d2c910e
JakPopielarz <popielarzjakub7@gmail.com>
2021-06-01 11:38:16 +0200

    Added file preparation (removing preamble) step

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import sys
 
 def main(argv):
     if len(sys.argv) > 1:
-        input = FileStream(argv[1])
+        prepared_file = prepare_input(argv[1])
         if len(sys.argv) > 2:
             output_name = argv[2]
         else:
             output_name = None
-        lexer = LatexLexer(input)
+        lexer = LatexLexer(InputStream(prepared_file))
         stream = CommonTokenStream(lexer)
         parser = LatexParser(stream)
         tree = parser.latexDocument()
@@ -21,6 +21,12 @@ def main(argv):
     else:
         print('Error : Expected a valid file')
 
+def prepare_input(file_name):
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        while "\\begin{document}" not in lines[0]:
+            lines.pop(0)
+        return ''.join(lines)
 
 # run, paste one example from examples.txt, and click ^D
 if __name__ == '__main__':
